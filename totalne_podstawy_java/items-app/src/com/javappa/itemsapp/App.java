339ed3e0a1af46b5cf61a87275907463ef7c8291
Adrian Wajs <wajsadrian@gmail.com>
2025-04-20 15:50:45 +0200

    Pozbycie sie zbednych plikow, gdyz zmieniam kurs

--- totalne_podstawy_java/items-app/src/com/javappa/itemsapp/App.java
+++
@@ -1,7 +0,0 @@
-package com.javappa.itemsapp;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ totalne_podstawy_java/items-app/src/com/javappa/itemsapp/Start.java
@@ -0,0 +1,7 @@
+package com.javappa.itemsapp;
+
+public class Start {
+    public static void main(String[] args){
+        System.out.println("System out println");
+    }
+}
